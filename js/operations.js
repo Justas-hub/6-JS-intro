039b3bae9b70f36a13e2752c748c29ede46dfb98
Justas-hub <63244160+Justas-hub@users.noreply.github.com>
2020-04-28 13:04:26 +0100

    Operations1

---
+++ js/operations.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+const sk1 = 27;
+const sk2 = 3;
+const sk3 = 8;
+
+console.log( sk1 );
+console.log( sk2 );
+console.log( sk3 );
+
+//Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+const vardas1 = 'Jonas';
+const vardas2 = 'Petras';
+const vardas3 = 'Zigmas';
+
+console.log( vardas1 );
+console.log( vardas2 );
+console.log( vardas3 );
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const skaiciai = [27, 3, 8];
+
+console.log( skaiciai );
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const vardai = ['Petras','Jonas','Zigmas'];
+
+console.log( vardai );
